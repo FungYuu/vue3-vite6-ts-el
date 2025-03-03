2d35856e144914b22587e6d52a7e995c618d7d2c
Kylin <1159469891@qq.com>
2025-03-03 11:34:56 +0800

    build: 配置vue-router

---
+++ src/router/index.ts
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
+
+import * as PAGE_URL from '@/constants/page-url-constants';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: PAGE_URL.PAGE_URL_HOME
+  },
+
+  {
+    path: PAGE_URL.PAGE_URL_HOME,
+    name: PAGE_URL.PAGE_URL_HOME,
+    component: () => import('@/App.vue')
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+// 添加全局前置守卫
+router.beforeEach((to, from, next) => {
+  next();
+});
+
+router.afterEach((to) => {
+  // 修改当前标签页的名称
+  const title = to.meta.title;
+  const titleNode = document.querySelector('head > title');
+  if (title && titleNode) {
+    titleNode.textContent = title.toString();
+  }
+});
+
+export default router;
