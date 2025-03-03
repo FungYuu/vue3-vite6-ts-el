2fdb3e7169616dffd42a412e932a46d97a7320db
Kylin <1159469891@qq.com>
2025-03-03 11:12:32 +0800

    build: 配置element-plus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+
+  <el-button type="primary">Primary</el-button>
+
+  <el-icon><Plus /></el-icon>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 import path from 'path';
 const pathSrc = path.resolve(__dirname, 'src');
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [ElementPlusResolver()],
+
+      dts: path.resolve(pathSrc, 'auto-imports.d.ts')
+    }),
+    Components({
+      resolvers: [
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver()
+      ],
+
+      dts: path.resolve(pathSrc, 'components.d.ts')
+    })
+  ],
   //这里进行配置别名
   resolve: {
     alias: {
