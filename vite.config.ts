7694803d4ae1324754260d96a1b61df714ab77a6
Kylin <1159469891@qq.com>
2025-03-03 10:46:34 +0800

    chore: 配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+import path from 'path';
+const pathSrc = path.resolve(__dirname, 'src');
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  //这里进行配置别名
+  resolve: {
+    alias: {
+      '@': pathSrc // @代替src
+    }
+  }
+});
