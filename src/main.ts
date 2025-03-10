212f09031a6c3adaac267e395769f9f6d48bca49
Kylin <1159469891@qq.com>
2025-03-10 17:45:56 +0800

    build: 移除element-plus全局引入

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 
 import 'normalize.css';
 import '@/assets/css/base.scss';
-import ElementPlus from 'element-plus';
+// import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 // eslint-disable-next-line import/order
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
@@ -14,7 +14,7 @@ import router from '@/router';
 
 const app = createApp(App);
 
-app.use(ElementPlus);
+// app.use(ElementPlus);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
